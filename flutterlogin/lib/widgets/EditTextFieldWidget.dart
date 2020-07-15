c5d1595a9b72a9a9854e59390749f9db8e6bb0a6
Colin_default <15910672817@163.com>
2020-07-15 17:50:37 +0800

    flutter 添加 登录页面效果 暂无功能交互

---
+++ flutterlogin/lib/widgets/EditTextFieldWidget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class EditTextFieldWidget extends StatelessWidget {
+  final ValueChanged<String> onSubmitted;
+  final VoidCallback onTab;
+  final String hintText;
+  final EdgeInsetsGeometry margin;
+
+  EditTextFieldWidget(
+      {Key key, this.hintText, this.onSubmitted, this.onTab, this.margin})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child:  Container(
+        margin: margin == null ? EdgeInsets.all(0.0) : margin,
+        width: MediaQuery.of(context).size.width,
+        alignment: AlignmentDirectional.center,
+        height: 50.0,
+//        decoration: BoxDecoration(
+//            color: Color.fromARGB(0, 237, 236, 237),
+//            borderRadius: BorderRadius.circular(24.0)),
+        child: TextField(
+          onSubmitted: onSubmitted,
+          onTap: onTab,
+          cursorColor: Color.fromARGB(255, 192, 191, 191),
+          decoration: InputDecoration(
+//            contentPadding: const EdgeInsets.only(top: 8.0),
+            border: InputBorder.none,
+            hintText: hintText,
+            hintStyle: TextStyle(
+                fontSize: 16, color: const Color(0xffc4c4c4)),
+//            prefixIcon: Icon(
+//              Icons.search,
+//              size: 25,
+//              color: Color.fromARGB(255, 128, 128, 128),
+//            )
+          ),
+          style: TextStyle(fontSize: 16,color: const Color(0xff323232)),
+        ),
+      )
+    );
+
+  }
+}
